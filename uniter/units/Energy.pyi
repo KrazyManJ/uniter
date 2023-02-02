3c3bbd155dab7c73c77e31d002439c354c7e274d
KrazyManJ <KrazyManJ@gmail.com>
2023-02-02 18:24:04 +0100

    Replace `Work` with `Energy` physical quantity

---
+++ uniter/units/Energy.pyi
@@ -0,0 +1,14 @@
+from uniter.Uniter import Unit, __Q
+
+class Energy(Unit[__Q]): pass
+
+class GJ(Energy[Energy]): pass
+
+class J(Energy[Energy]): pass
+
+class KJ(Energy[Energy]): pass
+
+class MJ(Energy[Energy]): pass
+
+class Work(Energy[Energy]): pass
+
